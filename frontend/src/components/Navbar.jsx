940e7a704df9b91510f02cd8700f8e414fe639d3
tarunfy <tarunsharma8920@gmail.com>
2022-07-12 15:48:51 +0530

    finished home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <header className="w-full">
+      <header className="w-full z-20">
         <nav className="flex items-center justify-between">
           <h1 className="text-4xl text-[#A3D8D7] font-Wotfard-Bold tracking-widest">
             ManageIt
           </h1>
-          <Link to="/login">
-            <a className="font-Wotfard-Regular text-xl">Login</a>
+          <Link to="/login" className="font-Wotfard-Regular text-xl">
+            Login
           </Link>
         </nav>
       </header>
